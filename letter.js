620ad6308db3f856bda330ead79c6ca28da2d02c
ronaldpannel <69686310+ronaldpannel@users.noreply.github.com>
2024-11-07 12:14:16 +0000

    Add files via upload

---
+++ letter.js
@@ -0,0 +1,48 @@
+class Letter {
+  constructor(x, y, fontSize) {
+    this.fontSize = fontSize;
+    this.pos = createVector(x, y);
+    this.vel = createVector(0, 0);
+    this.acc = createVector(0, 0);
+    this.sentence = [
+      "r",
+      "o",
+      "s",
+      "i",
+      "e",
+      "c",
+      "o",
+      "n",
+      "n",
+      "i",
+      "e",
+      "r",
+      "a",
+      "l",
+      "p",
+      "h",
+    ];
+    this.letter = random(this.sentence);
+    this.angle = 0;
+    this.angleV = 0.1;
+  }
+  applyForce(force) {
+    this.acc.add(force);
+  }
+  update() {
+    this.angle += this.angleV;
+    this.vel.add(this.acc);
+    this.pos.add(this.vel);
+    this.acc.mult(0);
+  }
+  draw() {
+    push();
+    textSize(this.fontSize);
+    textAlign(CENTER, CENTER);
+    translate(this.pos.x, this.pos.y);
+    rotate(this.angle);
+    text(this.letter, 0, 0);
+    pop();
+    fill(255);
+  }
+}
